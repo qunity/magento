5362a7fa5fc9318a033aa521da5a544e00adb383
Rodion Kachkin <r.kachkin@mygento.com>
2023-06-15 16:57:05 +0400

    Remove magento/module-admin-analytics package

--- app/etc/config.php
+++ app/etc/config.php
@@ -1,7 +1,6 @@
 <?php
 return [
     'modules' => [
-        'Magento_AdminAnalytics' => 1,
         'Magento_Store' => 1,
         'Magento_AdvancedPricingImportExport' => 1,
         'Magento_Directory' => 1,
