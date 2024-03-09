ef8a5df782356f9d033e88153c48b875d902c760
Rodion Kachkin <r.kachkin@mygento.com>
2024-03-10 02:41:37 +0400

    Update composer packages

--- app/etc/DotenvLoader.php
+++ app/etc/DotenvLoader.php
@@ -4,4 +4,4 @@ declare(strict_types=1);
 
 use Qunity\Component\Dotenv\Dotenv;
 
-Dotenv::create()->usePutEnv(true)->loadEnv(__DIR__ . '/../../dotenv/.env');
+Dotenv::create()->usePutEnv()->loadEnv(__DIR__ . '/../../dotenv/.env');
